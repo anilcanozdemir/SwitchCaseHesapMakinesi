7b983a6003b26f650d95af3374b97875c9bc5f48
ANIL CAN ÖZDEMİR <anilcanozdemir1020@gmail.com>
2022-04-20 16:35:49 +0300

    switchcasehesapmakinesi

--- src/SwitchCaseHesapMakinesi.java
+++ src/SwitchCaseHesapMakinesi.java
@@ -1,3 +1,43 @@
+import java.util.Scanner;
+
+/**
+ * @author ANIL CAN ÖZDEMİR
+ * Ödev
+ * Videodaki hesap makinesini switch-case kullanarak yapınız.
+ */
+
+
 public class SwitchCaseHesapMakinesi {
+    public static void main(String[] args) {
+        int n1,n2,select;
+        Scanner input =new Scanner(System.in);
+        System.out.println("Islem yapilacak iki sayiyi sirasiyla giriniz:");
+        n1=input.nextInt();
+        n2=input.nextInt();
+        System.out.println("1-Toplama\n2-Cikarma\n3-Carpma\n4-Bolme");
+        System.out.println("Seciminiz:");
+        select=input.nextInt();
+        switch (select)
+        {
+            case 1:
+                System.out.println(n1+n2);
+                break;
+            case 2:
+                System.out.println(n1-n2);
+                break;
+            case 3:
+                System.out.println(n1*n2);
+                break;
+            case 4:
+                if(n2==0)
+                {
+                    System.out.println("bir sayi sifira bölünemez");
+                    break;
+                }
+                System.out.println(n1/n2);
+                break;
+        }
+    }
+
 
 }
